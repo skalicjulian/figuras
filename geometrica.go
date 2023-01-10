5e710a9bb49065be6cdeabe7d1b8272431335d13
Skalic <jskalic@santandertecnologia.com.ar>
2023-01-10 10:41:25 -0300

    v2

--- geometrica.go
+++ geometrica.go
@@ -1,14 +1,14 @@
 package figuras
 
-type FigurasGeometricas interface {
+type figurasGeometricas interface {
 	area() float64
 	perimetro() float64
 }
 
-func CalcularArea(figura FigurasGeometricas) float64 {
+func CalcularArea(figura figurasGeometricas) float64 {
 	return figura.area()
 }
 
-func CalcularPerimetro(figura FigurasGeometricas) float64 {
+func CalcularPerimetro(figura figurasGeometricas) float64 {
 	return figura.perimetro()
 }
